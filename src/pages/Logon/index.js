f36e2a31b3d6c929a4a0574863a9142572eb317c
DanielSoaresRocha <legoliga123@gmail.com>
2020-05-21 01:59:48 -0300

    ajustando css

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css'
 
 function SideDrawer (props) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handlerLogon (e) {
+        e.preventDefault()
+    }
     return (
         <div className="containerLogon">
             <h1>Sistema de pedidos</h1>
             <img alt="imagem e-commerce" src={"https://news.comschool.com.br/wp-content/uploads/2014/09/ecommerce_sucesso.png"} />
 
-            <form>
+            <form onSubmit={handlerLogon}>
                 <div className="inputs">
-                    <input placeholder="Email" />
-                    <input type="password" placeholder="password" />
+                    <input
+                        placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)} />
+                    <input
+                        type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)} />
                 </div>
 
                 <div className="buttons">
