2f2e7749887f21b441c8103d5839f43a51fa9b7f
DanielSoaresRocha <legoliga123@gmail.com>
2020-05-21 01:26:39 -0300

    responsividade

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -8,10 +8,19 @@ function SideDrawer (props) {
             <h1>Sistema de pedidos</h1>
             <img alt="imagem e-commerce" src={"https://news.comschool.com.br/wp-content/uploads/2014/09/ecommerce_sucesso.png"} />
 
-            <div className="inputs">
-                <input placeholder="Email" />
-                <input type="password" placeholder="password" />
-            </div>
+            <form>
+                <div className="inputs">
+                    <input placeholder="Email" />
+                    <input type="password" placeholder="password" />
+                </div>
+
+                <div className="buttons">
+                    <button type="submit">Entrar</button>
+                    <div className="cadastrar">
+                        <button type="submit">Registrar</button>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 }
