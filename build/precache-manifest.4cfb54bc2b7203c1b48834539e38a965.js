00b8d243be95215061bb7d1cb4df28b17d2aad8d
DanielSoaresRocha <legoliga123@gmail.com>
2020-05-23 02:29:16 -0300

    adicionando buid

---
+++ build/precache-manifest.4cfb54bc2b7203c1b48834539e38a965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e5dbaa2d451621b022634c735860aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f7d5ae95f3295c10de",
+    "url": "/static/css/main.8508934c.chunk.css"
+  },
+  {
+    "revision": "74a6e93c052f1b6e2336",
+    "url": "/static/js/2.46b66a78.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.46b66a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f7d5ae95f3295c10de",
+    "url": "/static/js/main.05dab617.chunk.js"
+  },
+  {
+    "revision": "f7d3d6f23485dc87cf28",
+    "url": "/static/js/runtime-main.37e30bd7.js"
+  }
+]);
