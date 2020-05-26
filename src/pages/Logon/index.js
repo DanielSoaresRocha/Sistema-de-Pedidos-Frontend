c108215ab66565103500d4434e879b9a207b58d2
DanielSoaresRocha <legoliga123@gmail.com>
2020-05-25 21:53:51 -0300

    função recursiva

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -9,11 +9,20 @@ function SideDrawer (props) {
 
     const history = useHistory()
 
-    useEffect(() => {
+    const sleep = (milliseconds) => {
+        console.log('opa')
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+
+    async function effectLogon () {
+        console.log("foi")
         const randon = (min, max) => Math.floor(Math.random() * (max - min) + min)
 
         const ulSquares = document.querySelector("ul.squares")
-
+        if (!ulSquares)
+            return
+        ulSquares.innerHTML = ''
+        console.log(ulSquares)
         for (let i = 0; i <= 10; i++) {
             const li = document.createElement("li")
             let size
@@ -26,15 +35,17 @@ function SideDrawer (props) {
             li.style.width = `${size}px`
             li.style.height = `${size}px`
 
-            li.style.left = `${randon(1, 99)}%`
+            li.style.left = `${randon(1, 80)}%`
             li.style.animationDuration = `${randon(5, 30)}s`
             li.style.animationDelay = `${randon(0.1, 5)}`
-            li.style.overflow = "hidden"
-            ulSquares.style.overflow = "hidden"
-
             ulSquares.appendChild(li)
         }
-    }, [])
+        await sleep(10 * 1000)
+        effectLogon()
+    }
+    useEffect(() => {
+        effectLogon()
+    })
 
     function handlerLogon (e) {
         e.preventDefault()
@@ -42,34 +53,32 @@ function SideDrawer (props) {
     }
 
     return (
-        <>
+        <div className="containerLogon">
             <ul className="squares" />
-            <div className="containerLogon">
-                <h1>Sistema de pedidos</h1>
-                <img alt="imagem e-commerce" src={"https://news.comschool.com.br/wp-content/uploads/2014/09/ecommerce_sucesso.png"} />
+            <h1>Sistema de pedidos</h1>
+            <img alt="imagem e-commerce" src={"https://news.comschool.com.br/wp-content/uploads/2014/09/ecommerce_sucesso.png"} />
 
-                <form onSubmit={handlerLogon}>
-                    <div className="inputs">
-                        <input
-                            placeholder="Email"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)} />
-                        <input
-                            type="password"
-                            placeholder="password"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)} />
-                    </div>
+            <form onSubmit={handlerLogon}>
+                <div className="inputs">
+                    <input
+                        placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)} />
+                    <input
+                        type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)} />
+                </div>
 
-                    <div className="buttons">
-                        <button type="submit">Entrar</button>
-                        <div className="cadastrar">
-                            <button type="button" onClick={() => history.push('/register')}>Registrar</button>
-                        </div>
+                <div className="buttons">
+                    <button type="submit">Entrar</button>
+                    <div className="cadastrar">
+                        <button type="button" onClick={() => history.push('/register')}>Registrar</button>
                     </div>
-                </form>
-            </div>
-        </>
+                </div>
+            </form>
+        </div>
     );
 }
 
