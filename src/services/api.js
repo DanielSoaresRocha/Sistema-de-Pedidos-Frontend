7cfecbb9b79a321fddadbb021aa531ec1b2720e0
DanielSoaresRocha <legoliga123@gmail.com>
2020-05-26 13:00:25 -0300

    build

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    // baseURL: 'https://systemorders.herokuapp.com',
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: 'https://systemorders.herokuapp.com',
+    // baseURL: process.env.REACT_APP_API_URL,
 })
 
 export default api
