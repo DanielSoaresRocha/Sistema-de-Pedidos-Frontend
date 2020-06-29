16b04eb98b054fbbe3746e6df6657edd095c01ba
DanielSoaresRocha <legoliga123@gmail.com>
2020-06-28 23:42:12 -0300

    html da página FormPayment criado

--- src/pages/FormPayment/index.js
+++ src/pages/FormPayment/index.js
@@ -6,11 +6,43 @@ import HeaderNavigate from '../../components/HeaderNavigate';
 
 function FormPayment () {
     const history = useHistory()
+
+    let billet = true
     return (
         <HeaderNavigate name={'Forma de pagamento'} navigate={() => history.push('/get-adress')}>
             <div className='containerPayment'>
                 <br />
                 <span>Selecione uma forma de pagamento</span>
+                <div className='selectPayment'>
+                    <label
+                        htmlFor='cartao'>
+                        Pagamento com cartão
+                         </label>
+                    <input
+                        id='cartao'
+                        value='cartao'
+                        name="gender"
+                        type="radio"
+                    />
+                    <label
+                        htmlFor='boleto'>
+                        Pagamento com boleto
+                        </label>
+                    <input
+                        id='boleto'
+                        value='boleto'
+                        name="gender"
+                        type="radio"
+                    />
+                </div>
+                <div className='numPlots'>
+                    {billet === true ? (
+                        <select>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                        </select>
+                    ) : console.log('ok')}
+                </div>
             </div>
         </HeaderNavigate>
     )
