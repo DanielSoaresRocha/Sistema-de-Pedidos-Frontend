f463a1be2f8d8c9b5642d7f8487a277c85c4e127
DanielSoaresRocha <legoliga123@gmail.com>
2020-06-29 01:34:13 -0300

    layouth da tela formPayment finalizado

--- src/pages/FormPayment/index.js
+++ src/pages/FormPayment/index.js
@@ -8,57 +8,65 @@ function FormPayment () {
     const history = useHistory()
     const [billet, setBillet] = useState(false)
 
+    function handleFinish (e) {
+        e.preventDefault()
+
+        alert('vai para outra tela')
+    }
 
     return (
         <HeaderNavigate name={'Forma de pagamento'} navigate={() => history.push('/get-adress')}>
             <div className='containerPayment'>
                 <br />
                 <span>Tipo de pagamento</span>
-                <div className='selectPayment'>
-                    <input
-                        id='cartao'
-                        value='cartao'
-                        name="gender"
-                        type="radio"
-                        onChange={() => setBillet(false)}
-                    />
-                    <label
-                        htmlFor='cartao'>
-                        Pagamento com cartão
+                <form onSubmit={handleFinish}>
+                    <div className='selectPayment'>
+                        <input
+                            id='cartao'
+                            value='cartao'
+                            name="gender"
+                            type="radio"
+                            onChange={() => setBillet(false)}
+                            required={true}
+                        />
+                        <label
+                            htmlFor='cartao'>
+                            Pagamento com cartão
                          </label>
-                    <input
-                        id='boleto'
-                        value='boleto'
-                        name="gender"
-                        type="radio"
-                        onChange={() => setBillet(true)}
-                    />
-                    <label
-                        htmlFor='boleto'>
-                        Pagamento com boleto
+                        <input
+                            id='boleto'
+                            value='boleto'
+                            name="gender"
+                            type="radio"
+                            onChange={() => setBillet(true)}
+                            required={true}
+                        />
+                        <label
+                            htmlFor='boleto'>
+                            Pagamento com boleto
                         </label>
-                </div>
-                <div className='numPlots'>
-                    {billet === true ? (
-                        <>
-                            <h3>Nº parcelas no boleto</h3>
-                            <select>
-                                <option value='1'>1</option>
-                                <option value='2'>2</option>
-                                <option value='3'>3</option>
-                                <option value='4'>4</option>
-                                <option value='5'>5</option>
-                                <option value='6'>6</option>
-                                <option value='7'>7</option>
-                                <option value='8'>8</option>
-                                <option value='9'>9</option>
-                                <option value='10'>10</option>
-                            </select>
-                        </>
-                    ) : (<div />)}
-                </div>
-
-                <button type='button'>Próximo</button>
+                    </div>
+                    <div className='numPlots'>
+                        {billet === true ? (
+                            <>
+                                <h3>Nº parcelas no boleto</h3>
+                                <select>
+                                    <option value='1'>1</option>
+                                    <option value='2'>2</option>
+                                    <option value='3'>3</option>
+                                    <option value='4'>4</option>
+                                    <option value='5'>5</option>
+                                    <option value='6'>6</option>
+                                    <option value='7'>7</option>
+                                    <option value='8'>8</option>
+                                    <option value='9'>9</option>
+                                    <option value='10'>10</option>
+                                </select>
+                            </>
+                        ) : (<div />)}
+                    </div>
+                    <button type='submit'>Próximo</button>
+                </form>
             </div>
         </HeaderNavigate>
     )
