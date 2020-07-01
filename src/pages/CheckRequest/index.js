1a802e6004114e53acbd00ef75724a3f526879cf
DanielSoaresRocha <legoliga123@gmail.com>
2020-07-01 02:29:10 -0300

    criado html e css na sessão itensCarrinho

--- src/pages/CheckRequest/index.js
+++ src/pages/CheckRequest/index.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+
+import cart from '../../utils/cart'
 
 import './styles.css'
 import Home from '../../components/Home';
 
 function CheckRequest () {
+    // eslint-disable-next-line
     const { qtdBillets } = useParams()
+
+    useEffect(() => {
+        console.log(cart.getCart())
+    })
+
     return (
         <Home name={'Confira seu pedido'}>
-            <h1>eae men{qtdBillets}</h1>
+            <div className='checkContainer'>
+                <section>
+                    <h1>Itens do pedido</h1>
+                    <ul>
+                        {cart.getCart().map(item => (
+                            <li key={item.id}>
+                                <img
+                                    src={'https://comps.canstockphoto.com.br/produto-engrenagem-s%C3%ADmbolo-cardbox-desenho_csp53091492.jpg'}
+                                    alt={'item do carrinho'}
+                                />
+                                <div className='infoItem'>
+                                    <p>{item.nome}</p>
+                                    <p>{`$${item.preco}`}</p>
+                                    <p>{item.qtd}</p>
+                                </div>
+                                <div className='price'>{`$${item.preco * item.qtd}`}</div>
+                            </li>
+                        ))}
+                        <h1>Total: {`$${cart.getTotalCart()}`}</h1>
+                    </ul>
+                </section>
+            </div>
         </Home>
     )
 }
