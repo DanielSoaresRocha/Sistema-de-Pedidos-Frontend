85a3df5f553de13e3b316201315408de89ecf306
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-01 00:26:21 -0300

    adicionando acesso a camera

---
+++ src/components/Camera/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+
+function CameraComponent (props) {
+    function handleTakePhoto (dataUri) {
+        // Do stuff with the photo...
+        console.log('takePhoto');
+    }
+
+    return (
+        <Camera
+            onTakePhoto={(dataUri) => { handleTakePhoto(dataUri); }}
+        />
+    );
+}
+
+export default CameraComponent;
