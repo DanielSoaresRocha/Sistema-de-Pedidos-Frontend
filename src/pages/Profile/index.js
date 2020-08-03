df03d010a8a7e469adf3b82682fa0788c656d5b2
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-03 10:16:39 -0300

    páfina procfile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -13,6 +13,23 @@ function Profile () {
 
     return (
         <Home name={'Perfil'}>
+            <div className='containerProcfile'>
+                <img
+                    src='https://static.vecteezy.com/system/resources/previews/000/512/610/non_2x/profile-glyph-black-icon-vector.jpg'
+                    alt='Perfil'
+                />
+                <h1>Maria Silva</h1>
+                <span>denk@ufrn.edu.br</span>
+
+
+                <div className='changeProcfile'>
+                    <span>Enviar imagem de perfil</span>
+                    <button>Camera</button>
+                    <button>Arquivo</button>
+                </div>
+            </div>
+
+            {/*
             <Camera takePicture={takePicture} changeTakePicture={changeTakePicture} />
 
             {takePicture === true
@@ -20,7 +37,7 @@ function Profile () {
                 <div> </div>
                 :
                 <button class='takePicture' onClick={changeTakePicture}>Tirar outra foto</button>
-            }
+            }*/}
         </Home>
     );
 }
