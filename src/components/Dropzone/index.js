2564708796f62b4c0bcac534808e9ebcec3b6bf6
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-04 17:18:22 -0300

    adicionando react-dropzone para download de imagens

---
+++ src/components/Dropzone/index.js
@@ -0,0 +1,41 @@
+import React, { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { FiUpload } from 'react-icons/fi'
+
+import './styles.css'
+
+function Dropzone ({ onFileUploaded }) {
+    const [selectFileUrl, setSelectFileUrl] = useState('')
+
+    const onDrop = useCallback(acceptedFiles => {
+        const file = acceptedFiles[0]
+
+        const fileUrl = URL.createObjectURL(file)
+        setSelectFileUrl(fileUrl)
+        onFileUploaded(file)
+    }, [onFileUploaded])
+
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop,
+        accept: 'image/*'
+    })
+
+    return (
+        <div className="dropzone"{...getRootProps()}>
+            <input {...getInputProps()} accept='image/*' />
+
+            {selectFileUrl
+                ?
+                <img src={selectFileUrl} alt='Point' />
+                : (
+                    <p>
+                        <FiUpload />
+                Sua foto de perfil
+                    </p>
+
+                )}
+        </div>
+    )
+}
+
+export default Dropzone
